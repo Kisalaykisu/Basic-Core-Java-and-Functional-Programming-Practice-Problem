e9c23ad5390e07d8faaf17a9d3bb872aa2151283
Kisalay <kisalaykisu@gmail.com>
2022-06-19 18:40:50 +0530

    Basically my first commit

--- src/com/bridgelabz/CoreJava_Practice.java
+++ src/com/bridgelabz/CoreJava_Practice.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class CoreJava_Practice {
     public static void main(String[] args) {
-        System.out.println("Welcome to the playground of Java Coding : C");
+        System.out.println("Welcome to the playground of Java Coding : ");
     }
 }
