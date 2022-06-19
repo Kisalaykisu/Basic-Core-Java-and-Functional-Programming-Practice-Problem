82eebdf7db7ecd6df27bdcb41a82eebf08b2ceed
Kisalay <kisalaykisu@gmail.com>
2022-06-19 21:25:50 +0530

    Ninth commit has been added

--- src/com/bridgelabz/CoreJava_Practice.java
+++ src/com/bridgelabz/CoreJava_Practice.java
@@ -1,4 +1,4 @@
-/* Q-7. Java Program to Swap Two Numbers..
+/* Q-8 Java Program to Check Whether a Number is Even or Odd.
 */
 package com.bridgelabz;
 
@@ -6,20 +6,14 @@ import java.util.Scanner;  // import Scanner
 
 public class CoreJava_Practice {
     public static void main(String[] args) { // Entry Point of Program
-        Scanner scanner = new Scanner(System.in); // Make Scanner obj
-        System.out.println("Enter the two numbers to swap : ");
-        int x = scanner.nextInt();  // int input
-        int y = scanner.nextInt();
-        int swap;
+        Scanner scanner = new Scanner(System.in); //Make scanner obj
+        System.out.println("Enter the number : ");
+        int num = scanner.nextInt();  // Input int
 
-        System.out.println("Numbers before swapping are: "+ x + " "+ y);
-
-        swap = x;
-        x=y;
-        y=swap;
-
-        System.out.println("Numbers after swapping are: "+ x + " "+ y);
-
+        if(num%2==0){
+            System.out.println(num+" is an even number");
+        }else
+            System.out.println(num+" is an odd number");
         }
 
 }
