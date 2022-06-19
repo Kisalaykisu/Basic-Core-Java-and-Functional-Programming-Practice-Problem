1a291720eb5e536f73885754d997bf8af522886b
Kisalay <kisalaykisu@gmail.com>
2022-06-19 20:53:15 +0530

    Third commit has been added

--- src/com/bridgelabz/CoreJava_Practice.java
+++ src/com/bridgelabz/CoreJava_Practice.java
@@ -1,51 +1,32 @@
-/*      1. Flip Coin and print percentage of Heads and Tails
-        a. I/P -> The number of times to Flip Coin. Ensure it is a positive integer.
-        b. Logic -> Use Random Function to get value between 0 and 1. If < 0.5 then tails or
-        heads
-        c. O/P -> Percentage of Head vs Tails
+/*     Q-2. Leap Year
+         a. I/P -> Year, ensure it is a 4 digit number.
+         b. Logic -> Determine if it is a Leap Year.
+         c. O/P -> Print the year is a Leap Year or not.
 */
 package com.bridgelabz;
 
-import java.util.Random;   // import Random
 import java.util.Scanner;  // import Scanner
 
 public class CoreJava_Practice {
-    public static void main(String[] args) {  // Entry Point of Program
+    public static void main(String[] args) { // Entry Point of Program
+        int year = Year();
 
-        int heads = 0;  // Variable Creation
-        int tails = 0;
-        int coin;
-        int n;
-
-        Random random = new Random();  // Make Random obj
-        n = coinFlip();
-        while(n<=0) {
-            System.out.println("Enter a positive integer.");
-            n = coinFlip();
+        while(year<1000 || year>9999){      //  || represents logical Or
+            System.out.println("Enter a four digit number.");
+            year = Year();
         }
 
-        for(int i=0; i<n; i++) {
-
-            coin = random.nextInt(2);
-            switch (coin) {
-                case 0 -> heads++;
-                case 1 -> tails++;
-            }
-        }
-        System.out.println("Heads = " + heads);
-        System.out.println("Tails = " + tails);
-        System.out.println("Percentage Heads= "+(double)heads/n*100+" %");
-        System.out.println("Percentage Tails= "+(double)tails/n*100+" %");
-        if ((double)heads/n>0.5){
-            System.out.println("Its Heads");
-        }else
-            System.out.println("Its Tails");
-
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+            System.out.println("Specified year is a leap year");
+        else
+            System.out.println("Specified year is not a leap year");
     }
-    public static int coinFlip(){
-        Scanner scanner = new Scanner(System.in);  // Make Scanner Obj
-        System.out.println("Enter the number of times to flip the coin: ");
+
+    public static int Year(){
+        System.out.println("Enter an Year :: ");
+        Scanner scanner = new Scanner(System.in);  // create Scanner Object
         return scanner.nextInt();
+
     }
 
     }
