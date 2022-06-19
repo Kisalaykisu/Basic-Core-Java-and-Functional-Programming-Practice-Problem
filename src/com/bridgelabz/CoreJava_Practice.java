d390373b63bd1e258840baf913d5fc4f7fae6cf3
Kisalay <kisalaykisu@gmail.com>
2022-06-19 21:12:32 +0530

    Sixth commit :

--- src/com/bridgelabz/CoreJava_Practice.java
+++ src/com/bridgelabz/CoreJava_Practice.java
@@ -1,9 +1,8 @@
-/* Q-4. Harmonic Number
-a. Desc -> Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
-(http://users.encs.concordia.ca/~chvatal/notes/harmonic.html).
-b. I/P -> The Harmonic Value N. Ensure N != 0
-c. Logic -> compute 1/1 + 1/2 + 1/3 + ... + 1/N
-d. O/P -> Print the Nth Harmonic Value.
+/* Q-5. Factors
+      a. Desc -> Computes the prime factorization of N using brute force.
+      b. I/P -> Number to find the prime factors
+      c. Logic -> Traverse till i*i <= N instead of i <= N for efficiency.
+      d. O/P -> Print the prime factors of number N.
 */
 package com.bridgelabz;
 
@@ -11,23 +10,18 @@ import java.util.Scanner;  // import Scanner
 
 public class CoreJava_Practice {
     public static void main(String[] args) { // Entry Point of Program
-        int N = nthValue();
-        double nthHarmonic = 0;
+        int number;
+        Scanner scanner = new Scanner(System.in); // Make scanner Obj
+        System.out.println("Enter a number ::");
+        number = scanner.nextInt();
+        System.out.println("Prime factors are: ");
 
-        while (N==0){
-            System.out.println("Enter a value other than zero.");
-            N = nthValue();
+        for(int i = 2; i<= number; i++) {
+            while(number%i == 0) {
+                System.out.println(i);
+                number = number/i;
+            }
         }
-
-        for(int i=1;i<=N;i++){
-            nthHarmonic+=(double)1/i;
-            System.out.println(i+" th Harmonic Number is 1/"+i+" = "+ nthHarmonic);
-        }
-    }
-    public static int nthValue(){
-        Scanner scanner = new Scanner(System.in);  // Make Scanner object
-        System.out.println("Enter the value of N: ");
-        return scanner.nextInt();
 
     }
 
