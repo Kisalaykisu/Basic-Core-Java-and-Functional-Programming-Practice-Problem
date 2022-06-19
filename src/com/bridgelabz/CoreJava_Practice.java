cde48fca36147c38ced1dfa82702e9ca708e5a7f
Kisalay <kisalaykisu@gmail.com>
2022-06-19 21:22:23 +0530

    Eighth commit has been added:

--- src/com/bridgelabz/CoreJava_Practice.java
+++ src/com/bridgelabz/CoreJava_Practice.java
@@ -1,4 +1,4 @@
-/* Q-6. Java Program to Compute Quotient and Remainder.
+/* Q-7. Java Program to Swap Two Numbers..
 */
 package com.bridgelabz;
 
@@ -6,16 +6,20 @@ import java.util.Scanner;  // import Scanner
 
 public class CoreJava_Practice {
     public static void main(String[] args) { // Entry Point of Program
+        Scanner scanner = new Scanner(System.in); // Make Scanner obj
+        System.out.println("Enter the two numbers to swap : ");
+        int x = scanner.nextInt();  // int input
+        int y = scanner.nextInt();
+        int swap;
 
-        Scanner scanner = new Scanner(System.in);  // Make scanner obj
-        System.out.println("Enter the Dividend and Divisor: ");
-        int dividend = scanner.nextInt(); // Int input
-        int divisor = scanner.nextInt();
+        System.out.println("Numbers before swapping are: "+ x + " "+ y);
 
-        int quotient = dividend/divisor; // To find quotient
-        int remainder = dividend%divisor; // To find Remainder
+        swap = x;
+        x=y;
+        y=swap;
 
-        System.out.println("The Quotient is = "+quotient+" and the Remainder is = "+remainder);
+        System.out.println("Numbers after swapping are: "+ x + " "+ y);
+
         }
 
 }
